79ff316d3a14becfffb5ba8573e7e248a5e8279f
gustavoaquino <guuaquino@hotmail.com>
2021-05-29 12:54:04 -0300

    Inserindo regra de negocio apenas na service e retirando da controller.

--- src/main/java/br/com/rest/api/service/ProjetoService.java
+++ src/main/java/br/com/rest/api/service/ProjetoService.java
@@ -2,6 +2,7 @@ package br.com.rest.api.service;
 
 import br.com.rest.api.domain.Projeto;
 import br.com.rest.api.exceptions.ProjetoNaoEncontradoException;
+import br.com.rest.api.input.ProjetoInput;
 import br.com.rest.api.repository.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,14 @@ public class ProjetoService {
     @Autowired
     private ProjetoRepository projetoRepository;
 
+    public Projeto buscarUmProjeto(Long id) throws ProjetoNaoEncontradoException {
+        final Optional<Projeto> projetoOptional = this.projetoRepository.findById(id);
+        if(!projetoOptional.isPresent()){
+            throw new ProjetoNaoEncontradoException();
+        }
+        return projetoOptional.get();
+    }
+
     public List<Projeto> listarProjeto() throws ProjetoNaoEncontradoException {
         final List<Projeto> projetoList = this.projetoRepository.findAll();
         if(projetoList.isEmpty()){
@@ -23,19 +32,19 @@ public class ProjetoService {
         return this.projetoRepository.findAll();
     }
 
-    public Optional<Projeto> buscarUmProjeto(Long id) throws ProjetoNaoEncontradoException {
-        final Optional<Projeto> projetoOptional = this.projetoRepository.findById(id);
-        if(!projetoOptional.isPresent()){
-            throw new ProjetoNaoEncontradoException();
-        }
-        return this.projetoRepository.findById(id);
-    }
-
     public Projeto salvarProjeto(Projeto projeto){
         return this.projetoRepository.save(projeto);
     }
 
-    public void deletarProjeto(Projeto projeto) {
+    public Projeto atualizarProjeto(ProjetoInput projetoInput) throws ProjetoNaoEncontradoException {
+        final Projeto projetoNew = new Projeto(projetoInput);
+        Projeto projetoOld = this.buscarUmProjeto(projetoInput.getId());
+        projetoOld = projetoNew;
+        return this.projetoRepository.save(projetoOld);
+    }
+
+    public void deletarProjetoById(Long id) throws ProjetoNaoEncontradoException {
+        final Projeto projeto = this.buscarUmProjeto(id);
         this.projetoRepository.delete(projeto);
     }
 
